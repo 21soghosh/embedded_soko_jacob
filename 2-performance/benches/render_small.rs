8d1cb4eca7683fd937a0c96b2490f64ea3bedf01
Jacob Olsson <jacob.golsson@gmail.com>
2025-11-27 12:22:08 +0100

    IO removal, also benchmarks and profiling

--- 2-performance/benches/render_small.rs
+++ 2-performance/benches/render_small.rs
@@ -1,6 +1,10 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use rusttracer::render_dev;
 
+fn small_sample() -> Criterion {
+    Criterion::default().sample_size(10)
+}
+
 fn bench_render_small(c: &mut Criterion) {
     c.bench_function("render_dev_scene", |b| {
         b.iter(|| {
@@ -9,5 +13,9 @@ fn bench_render_small(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_render_small);
+criterion_group! {
+    name = benches;
+    config = small_sample();
+    targets = bench_render_small
+}
 criterion_main!(benches);

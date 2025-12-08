35e4123bec9e43bd896692c2e616598ea4b4f213
Soukarya Ghosh <s.ghosh-13@student.tudelft.nl>
2025-12-08 18:28:29 +0100

    ring buffer+mutex

--- 3-embedded/embedded-code/src/mutex.rs
+++ 3-embedded/embedded-code/src/mutex.rs
@@ -1,18 +1,28 @@
-use core::marker::PhantomData;
+use core::cell::UnsafeCell;
+use cortex_m::interrupt;
 
 pub struct Mutex<T> {
-    // TODO: remove this field. It's so it compiles.
-    phantom: PhantomData<T>,
+    data: UnsafeCell<T>,
 }
 
 impl<T> Mutex<T> {
     // make a new mutex
-    pub fn new(data: T) -> Self {
-        todo!()
+    pub const fn new(data: T) -> Self {
+        Self {
+            data: UnsafeCell::new(data),
+        }
     }
 
     // "locks" the mutex, and runs v as a critical section
     pub fn update<U>(&self, v: impl FnOnce(&mut T) -> U) -> U {
-        todo!()
+        interrupt::free(|_| {
+            let data = unsafe { &mut *self.data.get() };
+            v(data)
+        })
     }
 }
+
+
+
+unsafe impl<T> Sync for Mutex<T> where T: Send {}
+
--- 3-embedded/embedded-code/src/uart.rs
+++ 3-embedded/embedded-code/src/uart.rs
@@ -2,6 +2,54 @@ use core::fmt::Write;
 use tudelft_lm3s6965_pac::interrupt;
 use tudelft_lm3s6965_pac::UART0;
 
+use crate::mutex::Mutex;
+
+const BUFFER_SIZE: usize = 256;
+
+struct RingBuffer {
+    buffer: [u8; BUFFER_SIZE],
+    head: usize,
+    tail: usize,
+    full: bool,
+}
+
+impl RingBuffer {
+    pub const fn new() -> Self {
+        Self {
+            buffer: [0; BUFFER_SIZE],
+            head: 0,
+            tail: 0,
+            full: false,
+        }
+    }
+    fn is_empty(&self) -> bool {
+        if self.full {
+            false
+        } else if self.head == self.tail {
+            true
+        } else {
+            false
+        }
+    }
+    pub fn push(&mut self, byte: u8) {
+        self.buffer[self.head] = byte;
+        if self.full {
+            self.tail = (self.tail + 1) % BUFFER_SIZE;
+        }
+        self.head = (self.head + 1) % BUFFER_SIZE;
+        self.full = self.head == self.tail;
+    }
+    pub fn pop(&mut self) -> Option<u8> {
+        if self.is_empty() {
+            return None;
+        }
+        let byte = self.buffer[self.tail];
+        self.full = false;
+        self.tail = (self.tail + 1) % BUFFER_SIZE;
+        Some(byte)
+    }
+}
+
 pub struct Uart {}
 
 impl Uart {
